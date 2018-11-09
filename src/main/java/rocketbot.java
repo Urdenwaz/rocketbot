c4b52429361be7bf8c2b2ab42f0ab03ef8e5264c
Urdenwaz <urdenwaz@gmail.com>
2018-11-08 20:44:38 -0800

    lessened redundancies, changed bot prefix

--- src/main/java/rocketbot.java
+++ src/main/java/rocketbot.java
@@ -13,17 +13,16 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Random;
 
 public class rocketbot extends ListenerAdapter {
 
-    private List<String> design;
+    private String design;
 
     public static void main(String[] args) throws LoginException, IOException {
         try (BufferedReader rdr = new BufferedReader(new FileReader("token.txt"))) {
             JDA jda = new JDABuilder(AccountType.BOT)
                     .setToken(rdr.readLine())
-                    .setGame(Game.playing("with OpenRocket"))
+                    .setGame(Game.playing("type r!help for commands"))
                     .addEventListener(new rocketbot())
                     .build();
             String invite = jda.asBot().getInviteUrl();
@@ -33,11 +32,7 @@ public class rocketbot extends ListenerAdapter {
 
     public rocketbot() throws IOException {
         BufferedReader rdr2 = new BufferedReader(new FileReader("designfolder.txt"));
-        design = new ArrayList<>();
-        String line;
-        while ((line = rdr2.readLine()) != null) {
-            design.add(line.trim());
-        }
+        design = rdr2.readLine();
     }
 
     @Override
@@ -50,35 +45,35 @@ public class rocketbot extends ListenerAdapter {
         }
 
         else {
-            if (text.toLowerCase().equals("`help")) {
+            if (text.toLowerCase().equals("r!help")) {
                 EmbedBuilder bldr = new EmbedBuilder();
 
-                bldr.setAuthor("rocketbot Commands", "http://www.physicsrocks.com/rocketry-club.html", "https://cdn.discordapp.com/attachments/499820723192463370/504725105889378304/rocket.png");
+                bldr.setAuthor("rocketbot Commands", "https://github.com/Urdenwaz/rocketbot", "https://cdn.discordapp.com/attachments/499820723192463370/504725105889378304/rocket.png");
                 bldr.setThumbnail("https://cdn.discordapp.com/attachments/499820723192463370/504725105889378304/rocket.png");
                 bldr.setColor(Color.red);
 
-                bldr.addField("`rqdoc", "TARC Requirements Document", false);
-                bldr.addField("`specs", "TARC Rocket Specifications", false);
-                bldr.addField("`design", "Link to Current OpenRocket Design", false);
-                bldr.addField("`openrocket", "Link to OpenRocket", false);
+                bldr.addField("r!rqdoc", "TARC Requirements Document", false);
+                bldr.addField("r!specs", "TARC Rocket Specifications", false);
+                bldr.addField("r!design", "Link to Current OpenRocket Design", false);
+                bldr.addField("r!openrocket", "Link to OpenRocket", false);
 
                 e.getChannel().sendMessage(bldr.build()).complete();
             }
 
-            if (text.toLowerCase().equals("`rqdoc")) {
+            if (text.toLowerCase().equals("r!rqdoc")) {
                 e.getChannel().sendMessage("__**TARC Requirements Doc:**__").complete();
                 e.getChannel().sendMessage("https://3384f12ld0l0tjlik1fcm68s-wpengine.netdna-ssl.com/wp-content/uploads/2018/08/Event-Rules-TARC-2019-FINAL.pdf").complete();
             }
 
-            if (text.toLowerCase().equals("`design")) {
-                e.getChannel().sendMessage(design.get(0)).complete();
+            if (text.toLowerCase().equals("r!design")) {
+                e.getChannel().sendMessage(design).complete();
             }
 
-            if (text.toLowerCase().equals("`openrocket")) {
+            if (text.toLowerCase().equals("r!openrocket")) {
                 e.getChannel().sendMessage("http://openrocket.info/").complete();
             }
 
-            if (text.toLowerCase().equals("`specs")) {
+            if (text.toLowerCase().equals("r!specs")) {
                 EmbedBuilder bldr = new EmbedBuilder();
 
                 bldr.setAuthor("TARC Rocket Specifications", "https://rocketcontest.org/","https://cdn.discordapp.com/attachments/499820723192463370/504725105889378304/rocket.png");
@@ -93,18 +88,22 @@ public class rocketbot extends ListenerAdapter {
                 bldr.addField("Parachute", "Portion containing egg and altimeter must separate and descend with at least 2 parachutes", false);
                 bldr.addField("Flight Duration", "43 - 45 seconds, starting from lift-off", false);
                 bldr.addField("Flight Altitude", "856 feet", false);
-                bldr.addField("Flight Altitude According to Erin", "856 meters", false);
+                bldr.addField("Flight Altitude according to Erin", "856 meters", false);
 
                 e.getChannel().sendMessage(bldr.build()).complete();
             }
 
-            if (name.equals("Urdenwaz")) {
-                if (text.toLowerCase().equals("release the hounds")) {
-                    for (int i = 0; i < 1; i--) {
-                        e.getChannel().sendMessage("<message>").complete();
-                    }
-                }
+            if (text.toLowerCase().equals("f")) {
+                e.getChannel().sendMessage("https://cdn.discordapp.com/emojis/410317239859019776.gif?v=1").complete();
             }
+
+//            if (name.equals("Urdenwaz")) {
+//                if (text.toLowerCase().contains("`hounds")) {
+//                    for (int i = 0; i > 1; i--) {
+//                        e.getChannel().sendMessage("<@385299359581077506>").complete();
+//                    }
+//                }
+//            }
         }
     }
 }
